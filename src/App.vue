cb1cd6e51f80dcbeec56ea7be5ab6efa57433105
Mariusz Szymański <mariusz@melonio.pl>
2021-09-22 13:27:32 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h2>VueAxios</h2>
+  <ul>
+    <li v-for="country in info" :key="country.id">
+      {{ country.name }} | {{ country.capital }} | {{ country.area }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      info: [],
+    };
+  },
+  async mounted() {
+    const { data } = await this.axios.get(
+      "https://flask.wiejskomiejskie.pl/countries"
+    );
+    this.info = data;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
+
+createApp(App).use(VueAxios, axios).mount("#app");
